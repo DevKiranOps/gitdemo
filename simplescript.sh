d32e049d18d56fb24bd7039a1615b97e2eed883d
Kiran <kiran@yahoo.com>
2022-04-26 08:20:39 +0530

    let this be thereOC

--- simplescript.sh
+++ simplescript.sh
@@ -6,7 +6,7 @@ echo -e "Hi My name is $Name and I am from $Location.\n Today's date is $(date)"
 echo "Have a Good day"
 echo "This line should stay"
 echo "Maybe this one as well"
-
+echo "Hello Hello Hello"
 
 echo "Some random change needs to be deleted"
 
