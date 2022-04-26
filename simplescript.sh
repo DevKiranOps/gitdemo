7a1daa8fe96893bdff2494cb47f27e02d3cd4ed0
Kiran <kiran@yahoo.com>
2022-04-26 08:24:14 +0530

    oldnes replaced with new ones

--- simplescript.sh
+++ simplescript.sh
@@ -4,10 +4,4 @@ source vars.txt
 echo "Hello, World"
 echo -e "Hi My name is $Name and I am from $Location.\n Today's date is $(date)"
 echo "Have a Good day"
-
-echo "This may be using some randomebka;ldjhafldj "
-
-
-echo "This line should stay"
-
-echo "Maybe this one as well"
+echo "Some lines remvoed some lines added"
