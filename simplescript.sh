1e40dbaaca015ff43bcdd9839412d1c567d184f9
Kiran <kiran@yahoo.com>
2022-04-26 08:22:27 +0530

    resolved conflict

--- simplescript.sh
+++ simplescript.sh
@@ -4,11 +4,10 @@ source vars.txt
 echo "Hello, World"
 echo -e "Hi My name is $Name and I am from $Location.\n Today's date is $(date)"
 echo "Have a Good day"
+
+echo "This may be using some randomebka;ldjhafldj "
+
+
 echo "This line should stay"
+
 echo "Maybe this one as well"
-echo "Hello Hello Hello"
-echo "Let this stay. WIll this work?"
-
-echo "Some random change needs to be deleted"
-
-
